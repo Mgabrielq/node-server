3737fd5c5ca9d46e26b5fe363d6e6eada4c7d825
GabrielQueiroz <100012322+Mgabrielq@users.noreply.github.com>
2022-05-04 16:33:32 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Gabriel!!!</h1> <a href="/fabio">Go to Fabio</a>')
+})
+
+app.get('/fabio', (req, res) => {
+	const x = 1231221 + 4142;
+  res.send('<h1>Hello Fabio : ' + x + '</h1> <a href="/">Go to Gabriel</a>')
+})
+
+
+app.get('/info', (req, res) => {
+  res.sendFile('/home/devops/dev/myapp/gabriel.json')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
